032068e2d10f8b8506bc9d4b5d04f9687bdeadab
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2024-01-17 18:16:28 +0100

    change font size

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 static int topbar = 0;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"monospace:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
