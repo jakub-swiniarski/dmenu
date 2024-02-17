f9cb361a284331cbc7c1b79ab0c027869d255d4e
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2024-02-17 14:04:29 +0100

    default gray color

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#ffffff", "#000000" },
-	[SchemeSel] = { "#ffffff", "#960f96" },
+	[SchemeSel] = { "#ffffff", "#646464" },
 };
 
 static unsigned int lines = 0;
