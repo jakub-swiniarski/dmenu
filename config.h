553cd8e4744918389c59af062dfba92510e33ecb
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2024-05-02 15:35:05 +0200

    update color

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#ffffff", "#000000" },
-	[SchemeSel] = { "#ffffff", "#646464" },
+	[SchemeSel]  = { "#ffffff", "#0096c8" },
 };
 
 static unsigned int lines = 0;
