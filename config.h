30f33498012c59d3c67681354248ea8fdde90616
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2024-07-18 11:58:33 +0200

    Update config.h

--- config.h
+++ config.h
@@ -2,7 +2,7 @@ static int topbar = 0;
 static const char *fonts[] = {
 	"monospace:size=12"
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *prompt = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#ffffff", "#000000" },
